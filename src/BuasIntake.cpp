06fc5fc5a31d51e3df18083272050f8a0cdeaede
Tygo <tygoboons@hotmail.nl>
2022-10-12 11:34:27 +0200

    Added out folder to .gitignore

---
+++ src/BuasIntake.cpp
@@ -0,0 +1,12 @@
+﻿// BuasIntake.cpp : Defines the entry point for the application.
+//
+
+#include "BuasIntake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
